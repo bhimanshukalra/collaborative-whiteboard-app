4efd509de43987b612a4ddf611a2e0a6bcad06a1
bhimanshukalra <bhimanshukalra@gmail.com>
2025-10-25 21:41:04 +0530

    Chore: dashboard basic setup

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,24 @@
-import { Button } from "@/components/ui/button";
-import { SignedIn, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+"use client";
+
+import { useOrganization } from "@clerk/nextjs";
+import EmptyOrg from "./_components/empty-org";
+import { useSearchParams } from "next/navigation";
+import BoardList from "./_components/board-list";
 
 export default function Home() {
+  const { organization } = useOrganization();
+  const searchParams = useSearchParams();
+  const favourites = searchParams.get("favourites");
+  const search = searchParams.get("search");
+  const queryParams = { favourites, search };
+
   return (
-    <div className="flex flex-1 bg-amber-600 h-full">
-      <p>Dashboard root page</p>
+    <div className="flex-1 h-full p-6">
+      {organization ? (
+        <BoardList orgId={organization.id} query={queryParams} />
+      ) : (
+        <EmptyOrg />
+      )}
     </div>
   );
 }
